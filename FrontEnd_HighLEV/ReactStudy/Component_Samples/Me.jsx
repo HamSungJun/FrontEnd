5591c2ad3cd05d305e1202b4124d8aa52691b065
HamSungJun <wleo12345@naver.com>
2018-09-17 21:54:02 +0900

    리액트 라우터 해보겠습니다

---
+++ FrontEnd_HighLEV/ReactStudy/Component_Samples/Me.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+
+class Me extends React.Component{
+
+  componentDidMount(){
+    this.hungryTimerID = setInterval(()=>{
+      this.decreasingHungry();
+    },1500)
+    this.thirstyTimerID = setInterval(()=>{
+      this.decreasingThirsty();
+    },3000)
+  }
+
+  componentWillUnmount(){
+
+    clearInterval(this.hungryTimerID);
+    clearInterval(this.thirstyTimerID);
+
+  }
+  constructor(props){
+
+    super(props);
+    this.state = {
+      hungry : this.props.initialHungry,
+      thirsty : this.props.initialThirsty
+    }
+
+  }
+
+  decreasingHungry(){
+    this.setState({
+      hungry : (this.state.hungry-10)
+    })
+  }
+  decreasingThirsty(){
+    this.setState({
+      thirsty : (this.state.thirsty-50)
+    })
+  }
+
+  render(){
+    return(
+      <div>
+        <Monitor hungry={this.state.hungry} thirsty={this.state.thirsty} />
+      </div>
+    )
+  }
+}
+
+function Monitor(props) {
+  return(
+    <div>
+      <h1>Hungry : {props.hungry}</h1>
+      <h1>Thirsty : {props.thirsty}</h1>
+    </div>
+  )
+}
+
+export default Me
--- FrontEnd_HighLEV/ReactStudy/components/App.jsx
+++ FrontEnd_HighLEV/ReactStudy/components/App.jsx
@@ -1,68 +1,78 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter as Router , Route , Link} from 'react-router-dom';
 
 const root = document.getElementById('root');
 // stateless React Component
 
-class Me extends React.Component{
-
-  componentDidMount(){
-    this.hungryTimerID = setInterval(()=>{
-      this.decreasingHungry();
-    },1500)
-    this.thirstyTimerID = setInterval(()=>{
-      this.decreasingThirsty();
-    },3000)
-  }
-
-  componentWillUnmount(){
-
-    clearInterval(this.hungryTimerID);
-    clearInterval(this.thirstyTimerID);
-
-  }
-  constructor(props){
-
-    super(props);
-    this.state = {
-      hungry : this.props.initialHungry,
-      thirsty : this.props.initialThirsty
-    }
-
-  }
-
-  decreasingHungry(){
-    this.setState({
-      hungry : (this.state.hungry-10)
-    })
-  }
-  decreasingThirsty(){
-    this.setState({
-      thirsty : (this.state.thirsty-50)
-    })
-  }
-
+class App extends React.Component{
   render(){
     return(
       <div>
-        <Monitor hungry={this.state.hungry} thirsty={this.state.thirsty} />
+        <BasicRouter />
       </div>
     )
   }
 }
 
-function Monitor(props) {
+const BasicRouter = () => {
   return(
-    <div>
-      <h1>Hungry : {props.hungry}</h1>
-      <h1>Thirsty : {props.thirsty}</h1>
-    </div>
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/topics">Topics</Link>
+          </li>
+        </ul>
+
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/topics" component={Topics} />
+      </div>
+    </Router>
   )
 }
-const Header = (
+
+const Home = () => (
   <div>
-    <Me initialHungry={3000} initialThirsty={5000} />
+    <h1>Home</h1>
   </div>
 )
 
-ReactDOM.render(Header,root);
+const About = () => (
+  <div>
+    <h1>About</h1>
+  </div>
+)
+
+const Topics = ({match}) => (
+  <div>
+    <h1>Topics</h1>
+    <ul>
+      <li><Link to={`${match.url}/rendering`}>Rendering With React</Link></li>
+    </ul>
+    <ul>
+      <li><Link to={`${match.url}/components`}>Components</Link></li>
+    </ul>
+    <ul>
+      <li><Link to={`${match.url}/props-v-state`}>Props v state</Link></li>
+    </ul>
+
+    <Route path={`${match.url}/:topicid`} component={Topic}/>
+    <Route exact path={match.url} render={()=>{<h3>plz select a topic</h3>}}/>
+  </div>
+)
+
+const Topic = ({match}) => (
+  <div>
+    <h3>{match.params.topicid}</h3>
+  </div>
+)
+
+ReactDOM.render(<App />,root);
